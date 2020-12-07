dac73646189f3e0fa08327da84540129ca55aab9
Alexander <thezerothousand@gmail.com>
2020-12-07 15:01:10 -0600

    fixed declerations

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ console.log( add(2, 4) )
 
 console.log("%c 2. Multiplication!!  multiply(2,9) ", 'background: #222; color: #bada55')
 function multiply(a, b) {
-    index = 0;
-    result = 0;
+    let index = 0;
+    let result = 0;
 
     while (index < b) {
        result = add(a, result)
@@ -22,12 +22,14 @@ console.log(multiply(2,9))
 
 console.log("%c 3. Exponentiation!  exponent(2, 8)", 'background: #222; color: #bada55')
 function exponent(x, n) {
-    index = 0;
-    result = 1;
+    let index = 0;
+    let result = 1;
     while (index < n) {
         result = multiply(x, result)
         // console.log(result)
+        console.log(x, n, index)
         index++
+        
     }
     return result
 }
@@ -35,8 +37,8 @@ console.log(exponent(2, 8))
 
 console.log("%c 4. Factorial!!", 'background: #222; color: #bada55')
 function factorial(n) {
-    index = 1;
-    result = 1;
+   let index = 1;
+   let result = 1;
     while (index <= n) {
         result = multiply(index, result)
         // console.log(result)
